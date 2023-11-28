519018d968705b263b764dfe7460e872d2bf9da5
macmillianzungene <macmillianzungene@gmail.com>
2023-11-28 14:22:00 -0800

    printing ASCII alphabets excluding a number of some alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q'])))
+print(
+        "{}".format(
+            "".join(
+                chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q']
+                )
+            )
+        )
