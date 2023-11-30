133698b5547763b1360a5eb3671b219b16a93371
macmillianzungene <macmillianzungene@gmail.com>
2023-11-30 10:54:33 -0800

    importing funtions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module imports functions from calculator_1.py, performs some calculations, and prints the results.
+"""
+
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    main()
+
