3d6b78d6192eb7dccc2ae4c8dad456db40b13abf
macmillianzungene <macmillianzungene@gmail.com>
2023-11-30 09:20:21 -0800

    importing functions from files

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module imports the function add from add_0.py and prints the result of the addition 1 + 2 = 3.
+"""
+
+from add_0 import add
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
