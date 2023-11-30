84a6f1ef695f764b08e83c3152aeb4f99e96f996
macmillianzungene <macmillianzungene@gmail.com>
2023-11-30 14:11:21 -0800

    printing arguments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 This module imports functions from calculator_1.py, performs some calculations, and prints the results.
 """
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-def main():
+def print_args():
     num_args = len(sys.argv) - 1
-    if num_args == 1:
-        print("Number of argument: ", end="")
-    else:
-        print("Number of arguments: ", end="")
-
     if num_args == 0:
-        print(".")
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
     else:
-        print(f"{num_args}:")
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(f"{i}: {arg}")
+        print(f"{num_args} arguments:")
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
 
 if __name__ == "__main__":
-    main()
+    print_args()
 
