52cf754d1f0bc3e30b486b55638939ec38f3ae2d
macmillianzungene <macmillianzungene@gmail.com>
2024-01-09 14:55:25 -0800

    class Student that defines a student by

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module provides a Student class that defines a student by first name,
+last name, and age. It also includes a method to retrieve a dictionary
+representation of a Student instance.
+"""
+
+class Student:
+    """
+    A class that defines a student by first name, last name, and age.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with a first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        return self.__dict__
+
