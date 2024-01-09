8c72786ec70c85c56896d53eabacd758f37c9c15
macmillianzungene <macmillianzungene@gmail.com>
2024-01-09 14:26:42 -0800

    function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns the JSON representation of an object (string).
+"""
+
+def to_json_string(my_obj):
+    """
+    Function to return the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be serialized to a JSON string.
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
+    return str(my_obj)
+
