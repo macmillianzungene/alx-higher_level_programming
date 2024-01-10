13033773da0cccc7157cc446b7bd1496e9fbd849
macmillianzungene <macmillianzungene@gmail.com>
2024-01-10 15:11:13 -0800

    function that prints Python strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,31 +1,20 @@
 #include <Python.h>
 
-/**
- * print_python_string - Prints Python strings.
- * @p: PyObject pointer.
- *
- * Return: Nothing.
- */
 void print_python_string(PyObject *p)
 {
     long int length;
 
     /* Check if p is a string */
-    if (!PyUnicode_Check(p))
-    {
+    if (!PyUnicode_Check(p)) {
         printf("  [ERROR] Invalid String Object\n");
         return;
     }
 
-    /* Get string length */
+    /* Get the length of the string */
     length = PyUnicode_GET_LENGTH(p);
 
-    /* Print string type and length */
-    printf("  Type: %s\n  Length: %ld\n",
-           PyUnicode_KIND(p) == PyUnicode_1BYTE_KIND ? "compact ascii" : "compact unicode object",
-           length);
-
-    /* Print string as unicode object */
-    printf("  Value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+    /* Print the string */
+    printf("  length: %ld\n", length);
+    printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
 
