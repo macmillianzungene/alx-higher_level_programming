e5a4be50a5c5aa101275d41097f91971511793d8
macmillianzungene <macmillianzungene@gmail.com>
2024-01-15 15:57:25 -0800

    updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module for Square class"""
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
@@ -15,7 +16,21 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square.
+
+        Args:
+            value (int): The new size of the Square.
+        """
+        self.width = self.height = value
+
     def __str__(self):
         """Return an informal and nicely printable string of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
 
