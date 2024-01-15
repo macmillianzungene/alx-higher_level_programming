41958c17c59e3e6778cb53069632aba7cbf3164b
macmillianzungene <macmillianzungene@gmail.com>
2024-01-15 14:30:19 -0800

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,37 +1,79 @@
 #!/usr/bin/python3
-"""
-This module contains the Rectangle class that inherits from Base.
-"""
-
 from models.base import Base
 
 class Rectangle(Base):
-    """
-    This is the Rectangle class that inherits from Base.
-    """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        This is the constructor of the Rectangle class.
-
-        Args:
-            width (int): The width of the Rectangle instance.
-            height (int): The height of the Rectangle instance.
-            x (int, optional): The x of the Rectangle instance. Defaults to 0.
-            y (int, optional): The y of the Rectangle instance. Defaults to 0.
-            id (int, optional): The id of the Rectangle instance. Defaults to None.
-        """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    # ... existing code ...
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
 
     def display(self):
-        """
-        This method prints in stdout the Rectangle instance with the character #.
-        """
-        for _ in range(self.height):
-            print("#" * self.width)
+        for _ in range(self.__height):
+            print("#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        if args:
+            attributes = ["id", "width", "height", "x", "y"]
+            for attribute, value in zip(attributes, args):
+                setattr(self, attribute, value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
 
