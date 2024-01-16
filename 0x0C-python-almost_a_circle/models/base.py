d23c287bae6d5ba633466e0df5f47ba5c75102d6
macmillianzungene <macmillianzungene@gmail.com>
2024-01-15 16:37:46 -0800

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """
 This module contains the Base class.
 """
@@ -11,14 +12,21 @@ class Base:
 
     def __init__(self, id=None):
         """
-        This is the constructor of the Base class.
-
-        Args:
-            id (int, optional): The id of the Base instance. Defaults to None.
+        Constructor for the Base class.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
