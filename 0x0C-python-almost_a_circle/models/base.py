81ba0f8e345486a1629eca067d2b618e253ffbf8
macmillianzungene <macmillianzungene@gmail.com>
2024-01-15 16:50:20 -0800

    updated base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-This module contains the Base class.
+import json and os
 """
 import json
+import os
 """
 This module contains the Base class.
 """
@@ -32,4 +33,47 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+        """
+        filename = cls.__name__ + ".json"
+        list_dicts = []
+        if list_objs is not None:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string.
+        """
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set.
+        """
+        dummy = cls(1, 1)  # create a dummy instance
+        dummy.update(**dictionary)  # update the dummy instance with real values
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of instances.
+        """
+        filename = cls.__name__ + ".json"
+        if not os.path.exists(filename):
+            return []
+        with open(filename, 'r') as f:
+            list_dicts = cls.from_json_string(f.read())
+        return [cls.create(**dict_obj) for dict_obj in list_dicts]
 
