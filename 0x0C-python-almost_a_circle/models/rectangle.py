18a02597cd7c5ba0754e9cf0a5fb9738f9e46b4c
macmillianzungene <macmillianzungene@gmail.com>
2024-01-15 16:21:11 -0800

    updated rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,7 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if key in attributes:
                     setattr(self, key, value)
+
+     def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {"id": self.id, "width": self.__width, "height": self.__height, "x": self.__x, "y": self.__y}
