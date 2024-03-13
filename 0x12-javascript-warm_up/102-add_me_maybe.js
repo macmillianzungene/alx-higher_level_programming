1cb7dbdb09cccd513201c4840194726ae8952383
macmillianzungene <macmillianzungene@gmail.com>
2024-03-13 15:31:14 -0700

    a function that increments and calls a function.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-
-function incrementAndExecute(number, theFunction) {
-  let incrementedNumber = number + 1;
-  theFunction(incrementedNumber);
-}
-
-incrementAndExecute(5, (number) => console.log(`Incremented number is ${number}`));
-
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};
